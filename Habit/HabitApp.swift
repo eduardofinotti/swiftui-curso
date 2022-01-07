7aaa02c4590419b28dafd294c38ce7501a2fcf5b
Eduardo Finotti <eduardo.finotti@jamesdelivery.com.br>
2022-01-07 00:46:08 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Eduardo Finotti on 07/01/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
